441a4067574579768ea6c28613f6e8d932387685
mkutmon <mkutmon@gmail.com>
2013-05-03 21:46:18 +0200

    basic app framework with activator and menu action

---
+++ cytargetlinker/src/main/java/org/cytargetlinker/app/internal/CyActivator.java
@@ -0,0 +1,31 @@
+package org.cytargetlinker.app.internal;
+
+import java.util.Properties;
+
+import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.io.util.StreamUtil;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.osgi.framework.BundleContext;
+
+public class CyActivator extends AbstractCyActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+		CyApplicationManager cyApplicationManager = getService(context, CyApplicationManager.class);
+		CyNetworkViewFactory networkViewFactory = getService(context, CyNetworkViewFactory.class);
+		StreamUtil streamUtil = getService(context, StreamUtil.class);
+		CyNetworkFactory networkFactory = getService(context, CyNetworkFactory.class);
+		CyServiceRegistrar serviceRegistrar = getService(context, CyServiceRegistrar.class);
+
+		MenuAction action = new MenuAction(cyApplicationManager, "CyTargetLinker App");
+
+		Properties properties = new Properties();
+
+		registerAllServices(context, action, properties);
+	}
+
+}
---
+++ localbundles.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=bundles/org.bridgedb.jar -DgroupId=org.bridgedb -DartifactId=bridgedb -Dversion=1.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=bundles/org.bridgedb.bio.jar -DgroupId=org.bridgedb -DartifactId=bridgedb-bio -Dversion=1.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=bundles/derby.jar -DgroupId=derby -DartifactId=derby -Dversion=10.4 -Dpackaging=jar
