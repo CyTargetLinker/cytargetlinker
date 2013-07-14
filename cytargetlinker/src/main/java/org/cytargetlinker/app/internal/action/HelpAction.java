b73a6cd5c23d181cd95233ea36bbaf0d06f0384e
mkutmon <mkutmon@gmail.com>
2013-07-14 13:09:34 +0200

    added license header

--- cytargetlinker/src/main/java/org/cytargetlinker/app/internal/action/HelpAction.java
+++ cytargetlinker/src/main/java/org/cytargetlinker/app/internal/action/HelpAction.java
@@ -1,3 +1,20 @@
+// CyTargetLinker,
+// a Cytoscape plugin to extend biological networks with regulatory interaction
+//
+// Copyright 2011-2013 Department of Bioinformatics - BiGCaT, Maastricht University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package org.cytargetlinker.app.internal.action;
 
 import java.awt.event.ActionEvent;
